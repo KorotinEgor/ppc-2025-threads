e23021282d5f465e958abc94c5994ca4324334d5
Egor Korotin <korotin_e@master.nngu>
2025-05-13 01:29:06 +0300

    try without serial

--- tasks/all/korotin_e_crs_multiplication/func_tests/main.cpp
+++ tasks/all/korotin_e_crs_multiplication/func_tests/main.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
 #include <boost/mpi/communicator.hpp>
-#include <boost/mpi/environment.hpp>
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
@@ -60,6 +59,24 @@ void MatrixMultiplication(const std::vector<double> &a, const std::vector<double
         c[(i * p) + j] += a[(i * n) + k] * b[(k * p) + j];
       }
     }
+  }
+}
+
+void UselessFuncForTidyOne(const unsigned int n, std::vector<double> &a, std::vector<double> &b) {
+  for (unsigned int i = 0; i < (n * n) / 2; i++) {
+    a[i * 2] = 0;
+    b[(i * 2) + 1] = 0;
+  }
+}
+
+void UselessFuncForTidyTwo(const unsigned int n, std::vector<double> &a, std::vector<double> &b) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<unsigned int> a_distrib(0, (n * n) - 1);
+  std::uniform_int_distribution<unsigned int> b_distrib(0, (n * n) - 1);
+  for (unsigned int i = 0; i < (n * n) - n; i++) {
+    a[a_distrib(gen)] = 0;
+    b[b_distrib(gen)] = 0;
   }
 }
 
@@ -158,12 +175,7 @@ TEST(korotin_e_crs_multiplication_all, test_rndcrs_stat_zeroes) {
   if (world.rank() == 0) {
     a = korotin_e_crs_multiplication_all::GetRandomMatrix(m, n);
     b = korotin_e_crs_multiplication_all::GetRandomMatrix(n, p);
-    for (unsigned int i = 0; i < (m * n) / 2; i++) {
-      a[i * 2] = 0;
-    }
-    for (unsigned int i = 0; i < (n * p) / 2; i++) {
-      b[(i * 2) + 1] = 0;
-    }
+    korotin_e_crs_multiplication_all::UselessFuncForTidyOne(n, a, b);
 
     korotin_e_crs_multiplication_all::MakeCRS(a_ri, a_col, a_val, a, m, n);
     korotin_e_crs_multiplication_all::MakeCRS(b_ri, b_col, b_val, b, n, p);
@@ -203,8 +215,6 @@ TEST(korotin_e_crs_multiplication_all, test_rndcrs_stat_zeroes) {
   test_task_all.PostProcessing();
 
   if (world.rank() == 0) {
-    if (c_col.size() != out_col.size()) std::cout << "\n" << c_col.size() << "   " << out_col.size() << std::endl;
-
     ASSERT_EQ(c_ri, out_ri);
     ASSERT_EQ(c_col, out_col);
     ASSERT_EQ(c_val, out_val);
@@ -237,17 +247,7 @@ TEST(korotin_e_crs_multiplication_all, test_rndcrs) {
   if (world.rank() == 0) {
     a = korotin_e_crs_multiplication_all::GetRandomMatrix(m, n);
     b = korotin_e_crs_multiplication_all::GetRandomMatrix(n, p);
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<unsigned int> a_distrib(0, (m * n) - 1);
-    std::uniform_int_distribution<unsigned int> b_distrib(0, (n * p) - 1);
-    for (unsigned int i = 0; i < (m * n) - m; i++) {
-      a[a_distrib(gen)] = 0;
-    }
-    for (unsigned int i = 0; i < (n * p) - p; i++) {
-      b[b_distrib(gen)] = 0;
-    }
+    korotin_e_crs_multiplication_all::UselessFuncForTidyTwo(n, a, b);
 
     korotin_e_crs_multiplication_all::MakeCRS(a_ri, a_col, a_val, a, m, n);
     korotin_e_crs_multiplication_all::MakeCRS(b_ri, b_col, b_val, b, n, p);
@@ -287,8 +287,6 @@ TEST(korotin_e_crs_multiplication_all, test_rndcrs) {
   test_task_all.PostProcessing();
 
   if (world.rank() == 0) {
-    if (c_col.size() != out_col.size()) std::cout << "\n" << c_col.size() << "   " << out_col.size() << std::endl;
-
     ASSERT_EQ(c_ri, out_ri);
     ASSERT_EQ(c_col, out_col);
     ASSERT_EQ(c_val, out_val);
--- tasks/all/korotin_e_crs_multiplication/include/ops_all.hpp
+++ tasks/all/korotin_e_crs_multiplication/include/ops_all.hpp
@@ -22,6 +22,7 @@ class CrsMultiplicationALL : public ppc::core::Task {
   void MulTask(size_t l, size_t r, std::vector<double> &local_val, std::vector<unsigned int> &local_col,
                std::vector<unsigned int> &temp_r_i, const std::vector<unsigned int> &tr_i,
                const std::vector<unsigned int> &tcol, const std::vector<double> &tval);
+  void TrpB(std::vector<unsigned int> &tr_i, std::vector<unsigned int> &tcol, std::vector<double> &tval);
 
   std::vector<double> A_val_, B_val_, output_val_;
   std::vector<unsigned int> A_col_, A_rI_, B_col_, B_rI_, output_col_, output_rI_;
@@ -36,5 +37,9 @@ void MakeCRS(std::vector<unsigned int> &r_i, std::vector<unsigned int> &col, std
 
 void MatrixMultiplication(const std::vector<double> &a, const std::vector<double> &b, std::vector<double> &c,
                           unsigned int m, unsigned int n, unsigned int p);
+
+void UselessFuncForTidyOne(unsigned int n, std::vector<double> &a, std::vector<double> &b);
+
+void UselessFuncForTidyTwo(unsigned int n, std::vector<double> &a, std::vector<double> &b);
 
 }  // namespace korotin_e_crs_multiplication_all
